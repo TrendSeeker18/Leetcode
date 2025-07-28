f28ecb14c29f6512ff711b7b58c666aac798e58b
Nishtha <meharnishtha4@gmail.com>
2025-07-28 20:56:39 +0530

    Time: 35 ms (19.38%) | Memory: 62.9 MB (24.06%) - LeetSync

--- 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.java
+++ 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.java
@@ -1,30 +1,22 @@
 class Solution {
     public int maxProfit(int[] prices, int fee) {
-        int n = prices.length;
-        int[][] dp = new int[n][2]; // dp[i][buy]
-        for (int[] row : dp)
-            Arrays.fill(row, -1);
-        return solve(prices, 0, 1, fee, dp);
-    }
-
-    public int solve(int[] prices, int i, int buy, int fee, int[][] dp) {
-        if (i == prices.length) return 0;
-        if (dp[i][buy] != -1) return dp[i][buy];
-
-        int profit;
-        if (buy == 1) {
-            profit = Math.max(
-                -prices[i] + solve(prices, i + 1, 0, fee, dp),  // buy
-                solve(prices, i + 1, 1, fee, dp)                // skip
-            );
-        } else {
-            profit = Math.max(
-                prices[i] - fee + solve(prices, i + 1, 1, fee, dp),  // sell
-                solve(prices, i + 1, 0, fee, dp)                     // skip
-            );
+        int n=prices.length;
+        int [][] dp=new int [n+1][2];
+        for(int [] row:dp){
+            Arrays.fill(row,-1);
         }
-
-        dp[i][buy] = profit;
-        return profit;
+        return solve(prices,dp,fee,0,1);
+    }
+    private int solve(int [] prices,int [][] dp,int fee,int idx,int buy){
+        if (idx==prices.length) return 0;
+        if(dp[idx][buy]!=-1) return dp[idx][buy];
+        int profit=0;
+        if(buy==1){
+            profit=Math.max(-prices[idx]+solve(prices,dp,fee,idx+1,0),solve(prices,dp,fee,idx+1,1));
+        }
+        else{
+            profit=Math.max(prices[idx]+solve(prices,dp,fee,idx+1,1)-fee,solve(prices,dp,fee,idx+1,0));
+        }
+        return dp[idx][buy]=profit;
     }
 }
