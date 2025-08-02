31ac20a433a17624973094cd60df9eeb6b923659
Nishtha <meharnishtha4@gmail.com>
2025-08-02 16:46:52 +0530

    Time: 8 ms (60.47%) | Memory: 42.7 MB (67.73%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.java
+++ 567-permutation-in-string/permutation-in-string.java
@@ -1,24 +1,18 @@
-import java.util.*;
-
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        if (s1.length() > s2.length()) return false;
-
-        int[] pcount = new int[26];
-        int[] scount = new int[26];
-
-        for (int i = 0; i < s1.length(); i++) {
-            pcount[s1.charAt(i) - 'a']++;
-            scount[s2.charAt(i) - 'a']++;
+        int s1cnt[]=new int[26];
+        int s2cnt[] =new int [26];
+        for(int i=0;i<s1.length();i++){
+            s1cnt[s1.charAt(i)-'a']++;
         }
-
-        if (Arrays.equals(pcount, scount)) return true;
-
-        for (int i = s1.length(); i < s2.length(); i++) {
-            scount[s2.charAt(i) - 'a']++; // include right char
-            scount[s2.charAt(i - s1.length()) - 'a']--; // remove left char
-
-            if (Arrays.equals(pcount, scount)) return true;
+        for(int i=0;i<s2.length();i++){
+            s2cnt[s2.charAt(i)-'a']++;
+            if(i>=s1.length()){
+                s2cnt[s2.charAt(i-s1.length())-'a']--;
+            }
+            if(Arrays.equals(s1cnt,s2cnt)){
+                return true;
+            }
         }
         return false;
     }
